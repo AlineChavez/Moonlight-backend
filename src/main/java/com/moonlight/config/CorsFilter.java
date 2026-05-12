8bdf8ab2c5d1ebd8181be42f8cc2b7e9142f853c
Aline Chavez <alinechavez37@gmail.com>
2026-05-11 20:51:53 -0500

    fix: handle OPTIONS preflight in CorsFilter

--- src/main/java/com/moonlight/config/CorsFilter.java
+++ src/main/java/com/moonlight/config/CorsFilter.java
@@ -2,18 +2,30 @@ package com.moonlight.config;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
 
 @Provider
-public class CorsFilter implements ContainerResponseFilter {
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+            requestContext.abortWith(Response.ok()
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Headers", "Authorization, Content-Type")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .build());
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "Authorization, Content-Type");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
